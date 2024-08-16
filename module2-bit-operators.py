91af2b83c436ad6cf7235ca6eb8ea33edff92b8a
JudyLee8 <judyljaramillo@gmail.com>
2024-08-15 23:04:26 -0500

    Create module2-bit-operators.py

---
+++ module2-bit-operators.py
@@ -0,0 +1,19 @@
+first_bit = 1
+second_bit = 0
+
+print(first_bit & second_bit)
+
+first_bit = 0
+second_bit = 0
+
+print(first_bit | second_bit)
+
+first_bit = 1
+second_bit = 0
+
+print(first_bit ^ second_bit)
+
+print(~0)
+12 << 1
+12 << 2
+12 >> 2
