80b4a0f489530931c1081aecf2d33a0e38a851df
JudyLee8 <judyljaramillo@gmail.com>
2024-08-15 22:10:46 -0500

    Create module2-type-casting.py

---
+++ module2-type-casting.py
@@ -0,0 +1,7 @@
+height_cm = input('Height converter: enter your height in cm: ')
+float_height_cm = float(height_cm)
+print('Your height in feet is:', float_height_cm / 30.48)
+height_cm = float(input('Height converter: enter your height in cm: '))
+print('Your height in feet is:', height_cm / 30.48)
+year_born = int(input('What year were you born?'))
+print('In 2100, you will be', 2100 - year_born, 'years old, provided live this long!')
