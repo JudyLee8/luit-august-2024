a310558e7b4fbb4dcb7d0d364e169f06f7075d08
JudyLee8 <judyljaramillo@gmail.com>
2024-08-15 23:35:21 -0500

    Create module3-logical-operators-conditions.py

---
+++ module3-logical-operators-conditions.py
@@ -0,0 +1,11 @@
+password = input('Do you know the secret password? ')
+if password != '--secret':
+    print('not correct')
+else:
+     print('correct password')   
+if True:
+     print('Condition met')     
+if False:
+     print('Condition met') 
+if 2 == 2:
+     print('true')       
