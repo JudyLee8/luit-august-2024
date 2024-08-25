9fb37928b5afec9b5a481156b124f5165645d050
JudyLee8 <judyljaramillo@gmail.com>
2024-08-25 17:33:36 -0500

    Create module4-tuple-operations.py

---
+++ module4-tuple-operations.py
@@ -0,0 +1,16 @@
+user_data =('Joh', 'American', 1964)
+print(len(user_data))
+
+user_data =('Joh', 'American', 1964)
+if 'American' in user_data:
+    print('This person comes from the US!')
+
+user_data =('Joh', 'American', 1964)
+for element in user_data:
+    print(element)   
+
+user_data =('Joh', 'American', 1964) + ('teacher', 'male')
+print(user_data)
+
+numbers = (0, 1) * 10
+print(numbers)
