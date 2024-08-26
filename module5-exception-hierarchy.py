c8a20ffefd45a589de071c6384b09981baebfbad
JudyLee8 <judyljaramillo@gmail.com>
2024-08-26 00:39:05 -0500

    Create module5-exception-hierarchy.py

---
+++ module5-exception-hierarchy.py
@@ -0,0 +1,11 @@
+import sys
+
+user_name = input('What isyour name? ')
+if user_name == '':
+    print('Empty name? I cannot work with that. I am closing the program. Bye!')
+    sys.exit()
+print('Hello,', user_name)
+print('Let us get started...') 
+
+programing_language = ["Java", "Pythin", "C++"]
+print(programing_language[10])
