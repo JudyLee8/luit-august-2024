60319bae410a3dba2a635a4589df7b2ebacf4f28
JudyLee8 <judyljaramillo@gmail.com>
2024-09-02 19:39:42 -0500

    Update upload_new_object.py

--- upload_new_object.py
+++ upload_new_object.py
@@ -2,4 +2,7 @@ import boto3
 
 s3 = boto3.client('s3')
 
-s3.put_object(Bucket="zali-boto3-05012023", Key="test_text_string.txt", Body="Hey this is a string", ContentType="text/plain")
+s3.put_object(Bucket="zali-boto3-05012023", 
+                Key="folder/foldera/folder1/test_text_string.txt", 
+                Body="Hey this is a string", 
+                ContentType="text/plain")
